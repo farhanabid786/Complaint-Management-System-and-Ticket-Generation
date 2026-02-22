1a376749b9c2301f65e8885fbfd2f8d73d793ab7
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-02-23 05:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def ask_gemini(prompt):
 
         response = client.models.generate_content(
             model="gemini-1.5-flash",
-            contents=prompt
+            contents=prompt,
         )
 
         return response.text
